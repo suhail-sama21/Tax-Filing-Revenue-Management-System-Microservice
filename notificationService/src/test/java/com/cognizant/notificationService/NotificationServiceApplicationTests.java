8e54635e6a05bd4cad8d1cea4444e38344756519
suhail-sama21 <suhail.sama@cognizant.com>
2026-04-02 10:17:25 +0530

    "Changes notification project structure  "

---
+++ notificationService/src/test/java/com/cognizant/notificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.notificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
