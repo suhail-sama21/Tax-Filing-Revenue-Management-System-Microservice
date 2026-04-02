dc4b8e35a9e3b28e5862599f22470b004412fb6e
Harshith <Gangipelli.Harshith@cognizant.com>
2026-04-02 15:16:07 +0530

    microservices

---
+++ taxpayerRegistrationService/src/main/java/com/cognizant/taxpayerRegistrationService/TaxpayerRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.taxpayerRegistrationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxpayerRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxpayerRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ taxpayerRegistrationService/src/test/java/com/cognizant/taxpayerRegistrationService/TaxpayerRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.taxpayerRegistrationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxpayerRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
