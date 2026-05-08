d85d3a072e4ac3e67a8d6d343eecbb74c4f131ab
giridharang2-lang <Giridharan.G2@cognizant.com>
2026-05-09 01:01:15 +0530

    taxpayer and related changes

--- userService/src/main/java/com/cognizant/userService/dto/UpdateUserProfileRequest.java
+++ userService/src/main/java/com/cognizant/userService/dto/UpdateUserProfileRequest.java
@@ -1,10 +1,16 @@
 package com.cognizant.userService.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class UpdateUserProfileRequest {
+    private String name;
+    private String phone;
     private String address;
     private String panNumber;
     private LocalDate dob;
